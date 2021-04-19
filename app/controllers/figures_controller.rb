f0e839b284c8006553764f8b572daddea400743a
Jesse Wenger <wengerj@gmail.com>
2021-04-19 02:52:12 +0000

    add figures patch (still debugging)

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -25,10 +25,20 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.find_or_create_by(name: params[:figure][:name])
-    @landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
-    @title = Title.find_or_create_by(name: params[:title][:name])
-    @figure.landmarks << @landmark
-    @figure.titles << @title
+    #binding.pry
+    p_landmark_name = params[:landmark][:name]
+    p_landmark_year = params[:landmark][:year_completed]
+    if !p_landmark_name.empty?
+      @landmark = Landmark.find_or_create_by(name: p_landmark_name, year_completed: p_landmark_year)
+      @figure.landmarks << @landmark
+    end
+
+    p_title_name = params[:title][:name]
+    if !p_title_name.empty?
+      @title = Title.find_or_create_by(name: p_title_name)
+      add_figure_title = @figure.figure_titles.build(title: @title)
+      add_figure_title.save
+    end
 
     p_landmark_ids = params[:figure][:landmark_ids]
     if p_landmark_ids.size > 0
@@ -44,15 +54,66 @@ class FiguresController < ApplicationController
       p_title_ids.each do |title|
         p_title_id = title.to_i
         p_title = Title.find(p_title_id)
-        @figure.figure_titles.build(title: p_title)
+        new_figure_title = @figure.figure_titles.build(title_id: p_title_id)
+        new_figure_title.save
       end
     end
-    @figure.save
+
     redirect to "figures/#{@figure.id}"
   end
 
   patch '/figures/:id' do
     binding.pry
-    redirect "figures/#{@figure.id}"
+    @figure = Figure.find(params[:id])
+    @figure.update(name: params[:figure][:name])
+
+    p_landmark_name = params[:landmark][:name]
+    p_landmark_year = params[:landmark][:year_completed]
+    if !p_landmark_name.empty?
+      @landmark = Landmark.find_or_create_by(name: p_landmark_name, year_completed: p_landmark_year)
+      @figure.landmarks << @landmark
+    end
+
+    p_title_name = params[:title][:name]
+    if !p_title_name.empty?
+      @title = Title.find_or_create_by(name: p_title_name)
+      add_figure_title = @figure.figure_titles.build(title: @title)
+      add_figure_title.save
+    end
+
+    p_landmarks = params[:figure][:landmark_ids]
+    db_landmarks = @figure.landmarks
+
+    p_landmarks.each do |landmark|
+      p_landmark = landmark.to_i
+      if !db_landmarks.include?(p_landmark)
+        db_landmarks << p_landmark
+      end
+    end
+    db_landmarks.each do |landmark|
+      db_landmark = landmark.to_s
+      if !p_landmarks.include?(db_landmark)
+        db_landmarks.delete(landmark)
+      end
+    end
+
+    p_titles = params[:figure][:title_ids]
+    db_titles = @figure.titles
+
+    p_titles.each do |title|
+      p_title = title.to_i
+      if !db_titles.include?(p_title)
+        add_title = Title.find(p_title)
+        db_titles.figure_titles.build(title: add_title)
+      end
+    end
+    db_titles.each do |title|
+      db_title = title.to_s
+      if !p_titles.include?(db_title)
+        @figure.figure_titles.find(title_id: db_title)
+      end
+    end
+
+    redirect to "figures/#{@figure.id}"
   end
 end
