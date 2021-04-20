f65742b87bf416af319b2a98dfd241a1af967532
Jesse Wenger <wengerj@gmail.com>
2021-04-20 15:41:33 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -28,10 +28,10 @@ class FiguresController < ApplicationController
     @figure = Figure.find_or_create_by(name: params[:figure][:name])
 
     #New landmark
-    p_landmark_name = params[:landmark][:name]
-    p_landmark_year = params[:landmark][:year_completed]
-    if !p_landmark_name.empty?
-      @landmark = Landmark.find_or_create_by(name: p_landmark_name, year_completed: p_landmark_year)
+    landmark_name = params[:landmark][:name]
+    landmark_year = params[:landmark][:year_completed]
+    if !landmark_name.empty?
+      @landmark = Landmark.find_or_create_by(name: landmark_name, year_completed: landmark_year)
       @figure.landmarks << @landmark
     end
 
