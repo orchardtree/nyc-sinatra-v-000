1672c6d532ec70e6e62519ceb33f60d8c391b264
Jesse Wenger <wengerj@gmail.com>
2021-04-20 13:59:43 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -72,6 +72,9 @@ class FiguresController < ApplicationController
 
     @figure = Figure.find(params[:id])
 
+    #Update figure name
+    @figure.update(name: params[:figure][:name])
+
     #Update landmarks (checkboxes) if selections are made
     landmark_edits = params[:figure][:landmark_ids]
     landmarks = @figure.landmarks
@@ -145,10 +148,6 @@ class FiguresController < ApplicationController
       new_title_name = @figure.figure_titles.build(title: title)
       new_title_name.save
     end
-
-    #Update figure name
-    @figure.update(name: params[:figure][:name])
-    @figure.save
 
     #Redirect
     redirect to "figures/#{@figure.id}"
